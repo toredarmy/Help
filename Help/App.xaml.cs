e33f3cd81279dcd8aa465f037b85cbb8c528496b
toredarmy <to.red.army@gmail.com>
2022-05-10 11:43:05 +0300

    Run only one instance

--- Help/App.xaml.cs
+++ Help/App.xaml.cs
@@ -1,4 +1,46 @@
-﻿namespace Help
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+using Help.UI.View;
+
+namespace Help
 {
-    public partial class App { }
+    public partial class App
+    {
+        private const string UniqueEventName = "{GUID_HELP_EVENT_123}";
+        private const string UniqueMutexName = "{GUID_HELP_MUTEX_321}";
+        private EventWaitHandle _eventWaitHandle;
+        private Mutex _mutex;
+
+        private void AppOnStartup(object sender, StartupEventArgs e)
+        {
+            _mutex = new Mutex(true, UniqueMutexName, out var isOwned);
+            _eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+
+            GC.KeepAlive(_mutex);
+
+            if (isOwned)
+            {
+                var thread = new Thread(
+                    () =>
+                    {
+                        while (_eventWaitHandle.WaitOne())
+                        {
+                            Current.Dispatcher.BeginInvoke(
+                                (Action)(() => ((MainWindow)Current.MainWindow)?.BringToForeground()));
+                        }
+                    })
+                {
+                    IsBackground = true
+                };
+
+                thread.Start();
+                return;
+            }
+
+            _eventWaitHandle.Set();
+            Shutdown();
+        }
+    }
 }
--- Help/UI/View/MainWindow.xaml.cs
+++ Help/UI/View/MainWindow.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace Help.UI.View
+﻿using System.Windows;
+
+namespace Help.UI.View
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void BringToForeground()
+        {
+            if (WindowState == WindowState.Minimized || Visibility == Visibility.Hidden)
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            }
+            Activate();
+            Topmost = true;
+            Topmost = false;
+            Focus();
         }
     }
 }
