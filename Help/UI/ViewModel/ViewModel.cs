b292231ac1f6f4586f6887007a0921a7d2791dc3
toredarmy <to.red.army@gmail.com>
2022-05-10 11:47:47 +0300

    ViewModel

---
+++ Help/UI/ViewModel/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Help.UI.ViewModel
+{
+    internal class ViewModel
+    {
+        private string _title = "Help";
+        public string Title { get => _title; set => _title = value; }
+
+        public ViewModel()
+        {
+        }
+    }
+}
