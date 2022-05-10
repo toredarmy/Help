3b4df3af788b774aa45ca511409d8f777c420879
toredarmy <to.red.army@gmail.com>
2022-05-10 11:55:29 +0300

    Tray icon

--- Help/UI/View/MainWindow.xaml.cs
+++ Help/UI/View/MainWindow.xaml.cs
@@ -1,12 +1,33 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace Help.UI.View
 {
     public partial class MainWindow
     {
+        private readonly NotifyIcon _tray;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _tray = new NotifyIcon
+            {
+                Icon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location),
+                Text = "Help",
+                ContextMenu = new ContextMenu(new[]
+                {
+                    new MenuItem("Show", OnTrayShowClick),
+                    new MenuItem("Hide", OnTrayHideClick),
+                    new MenuItem("-"),
+                    new MenuItem("Exit", OnTrayExitClick),
+                }),
+                Visible = true,
+            };
+            _tray.MouseClick += OnTrayClick;
         }
 
         public void BringToForeground()
@@ -20,6 +41,58 @@ namespace Help.UI.View
             Topmost = true;
             Topmost = false;
             Focus();
+        }
+
+        private void OnTrayShowClick(object sender, EventArgs e)
+        {
+            BringToForeground();
+        }
+
+        private void OnTrayHideClick(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void OnTrayExitClick(object sender, EventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void OnTrayClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (Visibility == Visibility.Visible)
+                {
+                    Hide();
+                }
+                else
+                {
+                    BringToForeground();
+                }
+            }
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+#if !DEBUG
+            Hide();
+            e.Cancel = true;
+#endif
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _tray?.Dispose();
+            base.OnClosed(e);
         }
     }
 }
